328394b35bc4e979bf194a042df446add83bbcfa
Daniel <danielselga@gmail.com>
2021-08-04 02:02:12 -0300

    auth firebase

--- livechat/src/components/LoginForm.vue
+++ livechat/src/components/LoginForm.vue
@@ -2,23 +2,30 @@
   <form @submit.prevent="handleEvent">
     <input type="email" required placeholder="email" v-model="email">
     <input type="password" required placeholder="password" v-model="password">
+    <div class="error">{{ error }}</div>
     <button>Login</button>
   </form>
 </template>
 
 <script>
 import { ref } from '@vue/reactivity'
+import useLogin from '../composables/useLogin'
 
 export default {
-    setup(){
+    setup(props, context){ //o context vai substituir o this nesse caso, para emitirmos um evento vamos usar context.emit.
         const email = ref('')
         const password = ref('')
 
-        const handleEvent = () => {
-            console.log(email.value, password.value)
+        const {error, login} = useLogin()
+
+        const handleEvent = async () => {
+            await login(email.value, password.value)
+            if(!error.value){
+              context.emit('login')
+            }
         }
 
-        return {email, password, handleEvent}
+        return {email, password, handleEvent, error}
 
     }
 }
--- livechat/src/components/SignupForm.vue
+++ livechat/src/components/SignupForm.vue
@@ -3,24 +3,31 @@
     <input type="text" required placeholder="display name" v-model="displayName">
     <input type="email" required placeholder="email" v-model="email">
     <input type="password" required placeholder="password" v-model="password">
+    <div class="error">{{ error }}</div>
     <button>Sign up</button>
   </form>
 </template>
 
 <script>
 import { ref } from '@vue/reactivity'
+import useSignup from '../composables/useSignup'
 
 export default {
-    setup(){
+    setup(props, context){
+        const {error, signup} = useSignup()
+
         const displayName = ref('')
         const email = ref('')
         const password = ref('')
 
-        const handleEvent = () => {
-            console.log(displayName.value, email.value, password.value)
+        const handleEvent = async () => {
+            await signup(email.value, password.value, displayName.value)
+            if(!error.value){
+              context.emit('signup')
+            }
         }
 
-        return {displayName, email, password, handleEvent}
+        return {displayName, email, password, handleEvent, error, }
 
     }
 }
